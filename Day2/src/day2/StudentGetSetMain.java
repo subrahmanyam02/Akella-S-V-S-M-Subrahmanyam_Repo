4e6740d4ff9406704e902239215e493f7548a50e
AKELLA S V S M SUBRAHMANYAM <140712945+subrahmanyam02@users.noreply.github.com>
2024-08-30 15:23:52 +0530

    Update StudentGetSetMain.java

--- Day2/src/day2/StudentGetSetMain.java
+++ Day2/src/day2/StudentGetSetMain.java
@@ -4,7 +4,7 @@ public class StudentGetSetMain {
 	public static void main(String[] args) {
 		StudentGetSet obj=new StudentGetSet();
 		obj.setStdid(101);
-		obj.setStdname("veda");
+		obj.setStdname("Subrahmanyam");
 		System.out.println(obj);
 		}
 }
