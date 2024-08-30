35f4c2536b7af2ade20de56a7760bba2e5f56b2f
AKELLA S V S M SUBRAHMANYAM <140712945+subrahmanyam02@users.noreply.github.com>
2024-08-30 15:35:04 +0530

    Add files via upload

---
+++ Day2/src/day2/StudentsMain.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class StudentsMain {
+	public static void main(String[] args) {
+		Students student=new Students(99,"Subrahmanyam Akella");
+		System.out.println(student);
+		
+		
+	}
+
+}
